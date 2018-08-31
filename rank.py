2dee13de657196673a1de020824c781020d6e9dc
Joey Turczak <joeyturczak@gmail.com>
2018-08-31 10:29:06 -0700

    initial commit

---
+++ rank.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+root = tk.Tk()
+root.withdraw()
+current_directory = os.getcwd()
+
+def get_file_path():
+    file_path = filedialog.askopenfilename(initialdir = current_directory, 
+                                       title = 'Select file', 
+                                       filetypes = (('All files', '*.xls;*.xlsx;*.csv;*.htm'), 
+                                                    ('Excel files', '*.xls;*.xlsx'), 
+                                                    ('CSV files', '*.csv'), 
+                                                    ('HTM files', '*.htm')))
+    print('\nSelected file: {}'.format(file_path))
+    return file_path
+
+if __name__ == '__main__':
+    employee_list_file = get_file_path()
