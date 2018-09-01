a65825fdae67c164f2bf85f1a02fc96844ae4223
Joey Turczak <joeyturczak@gmail.com>
2018-08-31 18:36:46 -0700

    read data files

--- rank.py
+++ rank.py
@@ -1,24 +1,36 @@
 import pandas as pd
-import tkinter as tk
-from tkinter import filedialog
 import os
 
-root = tk.Tk()
-root.withdraw()
-current_directory = os.getcwd()
+cur_dir = os.getcwd()
+data_dir = cur_dir + '/data/'
+data_ext = (('.xls', '.xlsx', '.csv', '.htm'))
 
 def clean_dataset(df):
     return df
 
-def get_file_path():
-    file_path = filedialog.askopenfilename(initialdir = current_directory, 
-                                       title = 'Select file', 
-                                       filetypes = (('All files', '*.xls;*.xlsx;*.csv;*.htm'), 
-                                                    ('Excel files', '*.xls;*.xlsx'), 
-                                                    ('CSV files', '*.csv'), 
-                                                    ('HTM files', '*.htm')))
-    print('\nSelected file: {}'.format(file_path))
-    return file_path
+def get_files_list(directory='/', extensions='', abs_path=False):
+    file_dir = ''
+    if abs_path:
+        file_dir = directory
+    files = [file_dir + f for f in os.listdir(directory) if f.endswith(extensions)]
+    return files
+
+def get_dataset(filename):
+    print('\nLoading data from {}'.format(filename))
+    excel_ext = (('.xls', '.xlsx', '.htm'))
+    
+    if filename.endswith(excel_ext):
+        df = pd.read_excel(filename)
+    else:
+        df = pd.read_csv(filename)
+    print(df.head(1))
+    return df
+
 
 if __name__ == '__main__':
-    employee_list_file = get_file_path()
+    print('\n*******************************************************************')
+    print('******************************RANKING******************************')
+    print('*******************************************************************')
+    for f in get_files_list(directory=data_dir, extensions=data_ext, abs_path=True):
+        get_dataset(f)
+
