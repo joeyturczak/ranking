5e3b609cc804794a8a4c86d84542d4dcfb85177a
Joey Turczak <joeyturczak@gmail.com>
2018-08-31 20:38:12 -0700

    rename column headers

--- rank.py
+++ rank.py
@@ -5,8 +5,12 @@ cur_dir = os.getcwd()
 data_dir = cur_dir + '/data/'
 data_ext = (('.xls', '.xlsx', '.csv', '.htm'))
 
-def clean_dataset(df):
-    return df
+def clean_dataset(func):
+    def wrapper(*args, **kwargs):
+        df = func(*args, **kwargs)
+        df.rename(columns=lambda x: x.strip().lower().replace(' ', '_'), inplace=True)
+        return df
+    return wrapper
 
 def get_files_list(directory='/', extensions='', abs_path=False):
     file_dir = ''
@@ -15,14 +19,14 @@ def get_files_list(directory='/', extensions='', abs_path=False):
     files = [file_dir + f for f in os.listdir(directory) if f.endswith(extensions)]
     return files
 
+@clean_dataset
 def get_dataset(filename):
     print('\nLoading data from {}'.format(filename))
-    excel_ext = (('.xls', '.xlsx', '.htm'))
     
-    if filename.endswith(excel_ext):
-        df = pd.read_excel(filename)
-    else:
+    if filename.endswith('.csv'):
         df = pd.read_csv(filename)
+    else:
+        df = pd.read_excel(filename)
     print(df.head(1))
     return df
 
@@ -32,5 +36,5 @@ if __name__ == '__main__':
     print('******************************RANKING******************************')
     print('*******************************************************************')
     for f in get_files_list(directory=data_dir, extensions=data_ext, abs_path=True):
-        get_dataset(f)
+        print(get_dataset(f).head(1))
 
