06eb97228fd28a4d5cd800bdce5f8ad4322830c7
Joey Turczak <joeyturczak@gmail.com>
2018-09-06 23:52:59 -0700

    clean up code formatting

--- rank.py
+++ rank.py
@@ -68,13 +68,18 @@ def clean_dataset(func):
     def wrapper(*args, **kwargs):
         df = func(*args, **kwargs)
         # Strip whitespace from columns names and make them lowercase
-        df.rename(columns=lambda x: x.strip().lower().replace(' ', '_'), inplace=True)
-        df.rename(index=str, columns={'payroll_#':'payroll_number', 
-                                      'employee_number':'payroll_number',
-                                      'role_/_rate_effective_date':'role_date'}, inplace=True)
-        # Convert payroll_number column to string datatype and add leading zeroes
+        df.rename(columns=lambda x: x.strip().lower().replace(' ', '_'),
+                  inplace=True)
+        df.rename(index=str,
+                  columns={'payroll_#': 'payroll_number',
+                           'employee_number': 'payroll_number',
+                           'role_/_rate_effective_date': 'role_date'},
+                  inplace=True)
+        # Convert payroll_number column to string datatype and
+        # add leading zeroes
         if 'payroll_number' in df.columns:
-            df['payroll_number'] = df['payroll_number'].astype(str).apply('{0:0>6}'.format)
+            df['payroll_number'] = df['payroll_number'] \
+                .astype(str).apply('{0:0>6}'.format)
         return df
     return wrapper
 
@@ -90,43 +95,44 @@ def get_dataset(filename):
         df - Pandas DataFrame that has been loaded from file
     """
     print('\nLoading data from {}'.format(filename))
-    
+
     if filename.endswith('.csv'):
         df = pd.read_csv(filename)
     else:
         df = pd.read_excel(filename, header=None)
-        if 'Employee Settings Report' in df.iloc[0,0]:
+        if 'Employee Settings Report' in df.iloc[0, 0]:
             df = pd.read_excel(filename, header=5)
-        elif 'Virtual Roster Employee' in df.iloc[0,0]:
+        elif 'Virtual Roster Employee' in df.iloc[0, 0]:
             df = pd.read_excel(filename, header=3)
-        elif 'Leave Taken' in df.iloc[0,0]:
+        elif 'Leave Taken' in df.iloc[0, 0]:
             df = pd.read_excel(filename, header=7)
         else:
             df = pd.read_excel(filename)
     return df
-    
+
 
 def get_employee_info(file_prefix):
     """
     Combine employee data from multiple datasets into one dataset
 
     Returns:
-        df_combined - Pandas DataFrame that contains all relavent employee data for ranking
+        df_combined - Pandas DataFrame that contains all
+                      relavent employee data for ranking
     """
 
     employee_list_file = get_files_list(directory=dirs['emp_dir'],
-                           extensions=data_ext,
-                           startswith=file_prefix,
-                           abs_path=True)
+                                        extensions=data_ext,
+                                        startswith=file_prefix,
+                                        abs_path=True)
 
     attendance_file = get_files_list(directory=dirs['att_dir'],
-                           extensions=data_ext,
-                           startswith=file_prefix,
-                           abs_path=True)
+                                     extensions=data_ext,
+                                     startswith=file_prefix,
+                                     abs_path=True)
 
     demo_file = get_files_list(directory=dirs['demo_dir'],
-                           extensions=data_ext,
-                           abs_path=True)
+                               extensions=data_ext,
+                               abs_path=True)
 
     if not employee_list_file:
         error(errors['no_emp_list'])
@@ -147,9 +153,11 @@ def get_employee_info(file_prefix):
 
     df_att = calculate_points(attendance_file, df_role)
 
-    df_combined = pd.merge(df_role, df_att, how='left', on='payroll_number')
-    df_combined = pd.merge(df_combined, df_eval, how='left', on='payroll_number')
-    
+    df_combined = pd.merge(df_role, df_att,
+                           how='left', on='payroll_number')
+    df_combined = pd.merge(df_combined, df_eval,
+                           how='left', on='payroll_number')
+
     df_combined.fillna(0, inplace=True)
     df_combined['role_date'] = df_combined['role_date']
 
@@ -169,45 +177,70 @@ def calculate_rank(df):
     eval_pct = 0.7
     att_pct = 0.2
     role_pct = 0.1
-    
+
     # Attendance range: 0 to 12 reversed, increments of 0.5
-    att_range = np.array([12.0,11.5,11.0,10.5,10.0,9.5,9.0,8.5,8.0,7.5,7.0,6.5,6.0,5.5,5.0,4.5,4.0,3.5,3.0,2.5,2.0,1.5,1.0,0.5,0])
+    att_range = np.array([12.0, 11.5, 11.0, 10.5, 10.0, 9.5, 9.0, 8.5, 8.0,
+                          7.5, 7.0, 6.5, 6.0, 5.5, 5.0, 4.5, 4.0, 3.5, 3.0,
+                          2.5, 2.0, 1.5, 1.0, 0.5, 0])
     att_score = np.linspace(0, 1, att_range.size)
 
     # Eval score range: 1 to 5, increments of 0.2, includes 0.
     # Values over the max for the dataset are removed
-    eval_range = np.array([0,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.2,3.4,3.6,3.8,4.0,4.2,4.4,4.6,4.8,5.0])
-    eval_range = np.delete(eval_range, np.argwhere(eval_range > df['competency_score'].max()))
+    eval_range = np.array([0, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6,
+                           2.8, 3.0, 3.2,  3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6,
+                           4.8, 5.0])
+    eval_range = np.delete(eval_range,
+                           np.argwhere(eval_range > df['competency_score']
+                                       .max()))
     eval_score = np.linspace(0, 1, eval_range.size)
 
-    # Role date range: min role date to max role date reversed, increments of 1 day
-    role_date_range = pd.date_range(df['role_date'].min(), df['role_date'].max(), freq='D')[::-1]
-    role_date_len = len(pd.date_range(df['role_date'].min(), df['role_date'].max(), freq='D'))
+    # Role date range: min role date to max role date reversed,
+    # increments of 1 day
+    role_date_range = pd.date_range(df['role_date'].min(),
+                                    df['role_date'].max(), freq='D')[::-1]
+    role_date_len = len(pd.date_range(df['role_date'].min(),
+                                      df['role_date'].max(), freq='D'))
     role_score = np.linspace(0, 1, role_date_len)
-    
+
     # Lookup index of values from appropriate scale
-    df['att_score'] = df['points'].apply(lambda x: att_score[att_range == x]).astype(float)
-    df['eval_score'] = df['competency_score'].apply(lambda x: eval_score[eval_range == x]).astype(float)
-    df['role_score'] = df['role_date'].apply(lambda x: role_score[role_date_range==x]).astype(float)
+    df['att_score'] = df['points'] \
+        .apply(lambda x: att_score[att_range == x]).astype(float)
 
-    # Calculate total ranking score using percentage weights 
-    df['rank_score'] = df['att_score'] * att_pct + df['eval_score'] * eval_pct + df['role_score'] * role_pct
+    df['eval_score'] = df['competency_score'] \
+        .apply(lambda x: eval_score[eval_range == x]).astype(float)
 
-    #Separate employees into three groups: has an eval score, no eval score, contingent employees
+    df['role_score'] = df['role_date'] \
+        .apply(lambda x: role_score[role_date_range == x]).astype(float)
+
+    # Calculate total ranking score using percentage weights
+    df['rank_score'] = df['att_score'] * att_pct + df['eval_score'] \
+        * eval_pct + df['role_score'] * role_pct
+
+    # Separate employees into three groups: has an eval score,
+    # no eval score, contingent employees
     df_score = df.query('competency_score > 0')
-    df_noscore = df.loc[(df['competency_score'] == 0) & (df['payroll_number'].str.contains('^[0-9]+'))]
+
+    df_noscore = df.loc[(df['competency_score'] == 0) &
+                        (df['payroll_number'].str.contains('^[0-9]+'))]
+
     df_temp = df.loc[df['payroll_number'].str.startswith('C')]
 
     # Sort employees with an eval score by rank score
     df_score = df_score.sort_values(by=['rank_score'], ascending=False)
     df_score.reset_index(drop=True, inplace=True)
 
-    # Sort employees without an eval score by seniority, then attendance, then payroll number
-    df_noscore = df_noscore.sort_values(by=['role_score', 'att_score', 'payroll_number'], ascending=(False, False, True))
+    # Sort employees without an eval score by seniority,
+    # then attendance, then payroll number
+    df_noscore = df_noscore \
+        .sort_values(by=['role_score', 'att_score', 'payroll_number'],
+                     ascending=(False, False, True))
     df_noscore.reset_index(drop=True, inplace=True)
 
-    # Sort contingent employees without an eval score by seniority, then attendance, then payroll number
-    df_temp = df_temp.sort_values(by=['role_score', 'att_score', 'payroll_number'], ascending=(False, False, True))
+    # Sort contingent employees without an eval score by seniority,
+    # then attendance, then payroll number
+    df_temp = df_temp \
+        .sort_values(by=['role_score', 'att_score', 'payroll_number'],
+                     ascending=(False, False, True))
     df_temp.reset_index(drop=True, inplace=True)
 
     # Stitch all three groups back together and create a number rank
@@ -218,21 +251,27 @@ def calculate_rank(df):
 
     return df
 
+
 def get_eval_scores(df_role):
     """
     Retrieves eval scores for provided employees
 
     Args:
-        (pandas.DataFrame) df - dataset containing employee information including role start dates
+        (pandas.DataFrame) df - dataset containing employee information
+                                including role start dates
     Returns:
         df - Pandas DataFrame that includes performance scores
     """
     df_eval = pd.DataFrame()
-    files = get_files_list(directory=dirs['eval_dir'], extensions=data_ext, abs_path=True)
+
+    files = get_files_list(directory=dirs['eval_dir'],
+                           extensions=data_ext, abs_path=True)
+
     if not files:
         error(errors['no_eval_file'])
     for f in files:
         df_eval = df_eval.append(get_dataset(f))
+
     df_eval = df_eval[['payroll_number', 'competency_score']]
     df_eval.reset_index(drop=True, inplace=True)
 
@@ -252,11 +291,13 @@ def get_file_groups():
     Retrieves groups to be ranked together based on filenames
 
     Returns:
-        list - a list of groups derived from filenames of employee list files
+        list - a list of groups derived from filenames of
+               employee list files
     """
     groups = []
 
-    for f in get_files_list(directory=dirs['emp_dir'], extensions=data_ext, abs_path=True):
+    for f in get_files_list(directory=dirs['emp_dir'],
+                            extensions=data_ext, abs_path=True):
         groups.append(f.split('/')[-1].split('.')[0])
     if not groups:
         error(errors['no_emp_list'])
@@ -273,7 +314,8 @@ def get_employee_list(filepath):
         list - a list of employees
     """
     df_emp = get_dataset(filepath)
-    df_emp[['last_name', 'first_name']] = df_emp['name'].apply(lambda x: pd.Series(x.split(', ')))
+    df_emp[['last_name', 'first_name']] = df_emp['name'] \
+        .apply(lambda x: pd.Series(x.split(', ')))
     df_emp = df_emp[['payroll_number', 'last_name', 'first_name']]
     return df_emp
 
@@ -286,11 +328,13 @@ def get_role_dates(filepath, df_emp):
         (str) filepath - filepath of file containing employee role start dates
         (pandas.DataFrame) df - pandas DataFrame containing list of employees
     Returns:
-        df_combined - a pandas DataFrame with role start dates added to given employee list
+        df_combined - a pandas DataFrame with role start dates
+                      added to given employee list
     """
     df_emp_role = get_dataset(filepath)
     df_emp_role = df_emp_role[['payroll_number', 'role_date']]
-    df_combined = pd.merge(df_emp, df_emp_role, how='left', on='payroll_number')
+    df_combined = pd.merge(df_emp, df_emp_role,
+                           how='left', on='payroll_number')
     return df_combined
 
 
@@ -299,10 +343,13 @@ def calculate_points(filepath, df_role):
     Calculates points from attendance occurrence report
 
     Args:
-        (str) filepath - filepath of file containing employee attendance occurrences
-        (pandas.DataFrame) df - pandas DataFrame containing list of employees with role dates
+        (str) filepath - filepath of file containing employee
+                         attendance occurrences
+        (pandas.DataFrame) df - pandas DataFrame containing list of
+                                employees with role dates
     Returns:
-        df_point_totals - a pandas DataFrame with attendance point totals for given employees
+        df_point_totals - a pandas DataFrame with attendance point
+                          totals for given employees
     """
     df = get_dataset(filepath)
 
@@ -328,8 +375,11 @@ def calculate_points(filepath, df_role):
     df = df[['payroll_number', 'date', 'points']]
 
     # Get point totals for each employee
-    df_point_totals = df[['payroll_number', 'points']].groupby(['payroll_number']).sum().reset_index()
-    df_point_totals['points'] = df_point_totals['points'].apply(lambda x: 12 if x > 12 else x)
+    df_point_totals = df[['payroll_number', 'points']] \
+        .groupby(['payroll_number']).sum().reset_index()
+
+    df_point_totals['points'] = df_point_totals['points'] \
+        .apply(lambda x: 12 if x > 12 else x)
 
     return df_point_totals
 
@@ -366,7 +416,8 @@ def main():
         print('\n')
         print(df.describe())
 
-        filename = dirs['output_dir'] + group.split('/')[-1] + '_ranking_' + pd.Timestamp.now().strftime('%Y%m%d%H%M') + '.csv'
+        filename = dirs['output_dir'] + group.split('/')[-1] + '_ranking_' + \
+            pd.Timestamp.now().strftime('%Y%m%d%H%M') + '.csv'
 
         print('\nSaving data to file: {}'.format(filename))
         df.to_csv(filename, index=False)
